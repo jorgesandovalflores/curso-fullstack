6f992fe098115312eaedcea1d7f73da7092ee4ac
jorgesandoval <jorge@quebuu.com>
2023-08-10 20:41:28 -0500

    update clase 03

--- modulo-01/clase-03/patrones_diseno/factory_method.ts
+++ modulo-01/clase-03/patrones_diseno/factory_method.ts
@@ -1,23 +1,26 @@
+// Inteface
 interface Logger {
     log(message: string): void;
 }
 
+// Implementacion
 class ConsoleLogger implements Logger {
     log(message: string): void {
         console.log(`[Console] ${message}`);
     }
 }
-
 class FileLogger implements Logger {
     log(message: string): void {
         // Write log message to a file
     }
 }
 
+// Clase extendible Factory
 abstract class LoggerFactory {
     abstract createLogger(): Logger;
 }
 
+// Implmentacines Factory
 class ConsoleLoggerFactory extends LoggerFactory {
     createLogger(): Logger {
         return new ConsoleLogger();
--- modulo-01/clase-03/patrones_diseno/singleton.ts
+++ modulo-01/clase-03/patrones_diseno/singleton.ts
@@ -12,3 +12,5 @@ class DatabaseConnection {
         return DatabaseConnection.instance;
     }
 }
+
+const conection = DatabaseConnection.getInstance()
